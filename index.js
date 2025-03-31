fae08dff073d814ae70ca240dce9c8aedaa0560d
gmartinsfaria <g.martinsfaria@ua.pt>
2025-03-31 19:02:38 +0100

    Adicionei o carrossel das noticias

--- index.js
+++ index.js
@@ -70,6 +70,41 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
+// ================================================ NEWS ===============================================================
+
+document.addEventListener("DOMContentLoaded", function () {
+    const slider = document.querySelector(".news-scrolling-wrapper");
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    slider.addEventListener("mousedown", (e) => {
+        isDown = true;
+        slider.classList.add("active");
+        startX = e.pageX - slider.offsetLeft;
+        scrollLeft = slider.scrollLeft;
+    });
+
+    slider.addEventListener("mouseleave", () => {
+        isDown = false;
+        slider.classList.remove("active");
+    });
+
+    slider.addEventListener("mouseup", () => {
+        isDown = false;
+        slider.classList.remove("active");
+    });
+
+    slider.addEventListener("mousemove", (e) => {
+        if (!isDown) return;
+        e.preventDefault();
+        const x = e.pageX - slider.offsetLeft;
+        const walk = (x - startX) * 2; // Ajusta a velocidade do arrasto
+        slider.scrollLeft = scrollLeft - walk;
+    });
+});
+
+// ================================================ NEWS FIM ===============================================================
 
 
 //secção TEAM
