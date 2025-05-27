9c639a5885ebdce137a2b7311316ce062ccde954
gmartinsfaria <g.martinsfaria@ua.pt>
2025-05-27 19:05:35 +0100

    New arrows (news and team)

--- translations.js
+++ translations.js
@@ -24,26 +24,28 @@ const translations = {
 
     //NEWS --> NOTÍCIA 3
     news3Title: "Regimes de Casamento em Portugal: uma análise segundo a legislação portuguesa",
-    news3Description: "Artigo da autoria de Verónica Casimiro, colaboradora da RPA, acerca dos regimes de casamento em Portugal e publicado no Jornal Açores 9.",
+    news3Description: "Artigo da autoria de Verónica Casimiro, colaboradora da RPA, acerca dos regimes de casamento em Portugal.",
 
     //NEWS --> NOTÍCIA 4
     news4Title: "É só uma “brincadeira” do meu patrão!",
-    news4Description: "Artigo da autoria de Bianca Pereira, colaboradora da RPA, acerca do assédio laboral e publicado no Jornal Açoriano Oriental.",
+    news4Description: "Artigo da autoria de Bianca Pereira, colaboradora da RPA, acerca do assédio laboral.",
 
     //NEWS --> NOTÍCIA 5
     news5Title: "O meu filho tem más notas na universidade, tenho de pagar pensão de alimentos?",
-    news5Description: "Artigo da autoria de Júlia Filipe, colaboradora da RPA, acerca do enquadramento legal relativo à prestação da pensão de alimentos por parte dos progenitores e publicado no Jornal Açores 9 no âmbito da rubrica Clínica Jurídica.",
+    news5Description: "Artigo da autoria de Júlia Filipe, colaboradora da RPA, acerca do enquadramento legal relativo à prestação da pensão de alimentos por parte dos progenitores.",
 
     //NEWS --> NOTÍCIA 6
     news6Title: "A Marcação do Período de Férias: Um Equilíbrio entre Direito e Organização",
-    news6Description: "Artigo da autoria de João Baptista, colaboradora da RPA, acerca da marcação do período de férias e publicado no Jornal Açores 9 no âmbito da rubrica Clínica Jurídica.",
+    news6Description: "Artigo da autoria de João Baptista, colaboradora da RPA, acerca da marcação do período de férias.",
 
     //NEWS --> NOTÍCIA 7
     news7Title: "Interrupção da Gravidez e o Certificado de Incapacidade Temporária para o Trabalho",
-    news7Description: "Artigo da autoria de Bianca Pereira, colaboradora da RPA, acerca da IVG e publicado no Jornal Açores 9 no âmbito da rubrica Clínica Jurídica.",
+    news7Description: "Artigo da autoria de Bianca Pereira, colaboradora da RPA, acerca da IVG.",
 
-    knowMore: "Saiba mais na nossa Newsletter",
+    knowMore: "Ver todos",
 
+    //ACTIVITY
+    fieldListTitle: "Dentro desta área atuamos nas seguintes questões:",
 
     //ACTIVITY --> ADMINISTRATIVE
     administrativeField: "Contencioso Administrativo",
@@ -56,6 +58,29 @@ const translations = {
     taxFieldStart: "O ",
     taxFieldEnd: "trata das disputas legais entre contribuintes e o Estado relacionadas à aplicação das leis tributárias. Este ramo do direito envolve a resolução de conflitos sobre a obrigação de pagamento de tributos, a interpretação de normas fiscais, e a contestação de autuações fiscais e sanções impostas pelos órgãos tributários. O foco é garantir que a tributação seja justa e que as obrigações fiscais sejam cumpridas conforme a legislação vigente.",
     taxFieldSentence2: "Este campo inclui a defesa dos direitos do contribuinte em situações de cobrança indevida, contestação de impostos e outras medidas fiscais, além de regulamentar a atuação do Estado na fiscalização e arrecadação de tributos. O objetivo é assegurar um equilíbrio entre o cumprimento das obrigações fiscais e a proteção dos direitos dos contribuintes perante o sistema tributário.",
+  
+    //ACTIVITY --> COMERCIAL
+    comercialField: "Direito Comercial e Societários",
+    comercialFieldStart: "O ",
+    comercialFieldEnd: "regula as atividades económicas e empresariais, assegurando um quadro jurídico estável para a criação, gestão e dissolução de empresas. Este ramo do direito abrange as normas que regem as relações entre comerciantes, sociedades e demais agentes económicos, promovendo a segurança e transparência no mercado.",
+    comercialFieldSentence2: "Fundamental para o funcionamento das empresas, o Direito Comercial e Societário define as regras para a constituição de sociedades, contratos comerciais, responsabilidade dos sócios e administradores, bem como os mecanismos de resolução de conflitos empresariais. A sua aplicação visa garantir o equilíbrio nas relações comerciais e fomentar um ambiente de negócios sólido e competitivo.",
+    comercialFieldListItem1: "Contratos comerciais",
+
+    //ACTIVITY --> FAMILY
+    familyField: "Direito da Família",
+    familyFieldStart: "O ",
+    familyFieldEnd: "é o ramo do direito que regula as relações jurídicas entre membros de uma família, abordando questões essenciais como o casamento, a união de facto, a filiação, a guarda dos filhos, a adoção e a sucessão. Este campo do direito visa proteger os direitos individuais e coletivos dos membros da família, promovendo a convivência harmoniosa e o bem-estar das pessoas envolvidas. A legislação neste âmbito também lida com a proteção da criança, do idoso e de outros membros vulneráveis da família.",
+    familyFieldSentence2: "Além disso, o Direito da Família trata de questões como o regime de bens no casamento, a partilha de património, pensões alimentícias e a definição de responsabilidades parentais. O objetivo é resolver litígios familiares de forma equilibrada e justa, assegurando que os direitos de cada indivíduo sejam respeitados e que o melhor interesse das crianças seja sempre uma prioridade nas decisões judiciais.",
+    familyFieldListItem1: "Ações de Paternidade e de prestações de alimentos",
+    familyFieldListItem2: "Ações de Separação de Bens e de Pessoas e Bens",
+    familyFieldListItem3: "Adopção",
+    familyFieldListItem4: "Anulação do casamento católico",
+    familyFieldListItem5: "Deserdação",
+    familyFieldListItem6: "Divórcio por mútuo consentimento e sem consentimento de outro cônjuge",
+    familyFieldListItem7: "Mediação Familiar",
+    familyFieldListItem8: "Proteção de Crianças e Jovens em Risco",
+    familyFieldListItem9: "Responsabilidades Parentais",
+  
   },
   en: {
     
@@ -82,26 +107,28 @@ const translations = {
 
     //NEWS --> NOTÍCIA 3
     news3Title: "Marriage regimes in Portugal: an analysis according to Portuguese legislation",
-    news3Description: "Article by RPA contributor Verónica Casimiro on marriage regimes in Portugal, published in Açores 9 newspaper.",
+    news3Description: "Article by RPA contributor Verónica Casimiro on marriage regimes in Portugal.",
 
     //NEWS --> NOTÍCIA 4
     news4Title: "It's just a “joke” from my boss!",
-    news4Description: "Article by RPA contributor Bianca Pereira on workplace harassment, published in the Açoriano Oriental newspaper.",
+    news4Description: "Article by RPA contributor Bianca Pereira on workplace harassment.",
 
     //NEWS --> NOTÍCIA 5
     news5Title: "My son got bad grades at university, do I have to pay child support?",
-    news5Description: "Article by RPA contributor Júlia Filipe on the legal framework for maintenance payments by parents, published in the Açores 9 newspaper under the heading Legal Clinic.",
+    news5Description: "Article by RPA contributor Júlia Filipe on the legal framework for maintenance payments by parents.",
 
     //NEWS --> NOTÍCIA 6
     news6Title: "The scheduling of vacation time: a balance between law and organization",
-    news6Description: "Article by RPA contributor João Baptista on the scheduling of vacation time and published in the Açores 9 newspaper under the heading Legal Clinic.",
+    news6Description: "Article by RPA contributor João Baptista on the scheduling of vacation time.",
 
     //NEWS --> NOTÍCIA 7
     news7Title: "Termination of Pregnancy and the Certificate of Temporary Incapacity for Work",
-    news7Description: "Article by RPA contributor Bianca Pereira on Voluntary termination of pregnancy, published in the Açores 9 newspaper under the heading Legal Clinic.",
+    news7Description: "Article by RPA contributor Bianca Pereira on Voluntary termination of pregnancy.",
   
-    knowMore: "Find out more in our Newsletter",
+    knowMore: "See all",
 
+    //ACTIVITY
+    fieldListTitle: "Within this area we work on the following issues:",
 
     //ACTIVITY --> ADMINISTRATIVE
     administrativeField: "Administrative Litigation",
@@ -114,5 +141,27 @@ const translations = {
     taxFieldStart: " ",
     taxFieldEnd: "deals with legal disputes between taxpayers and the state relating to the application of tax laws. This branch of law involves resolving conflicts over the obligation to pay taxes, interpreting tax rules, and challenging tax assessments and penalties imposed by tax authorities. The focus is on ensuring that taxation is fair and that tax obligations are met in accordance with current legislation.",
     taxFieldSentence2: "This field includes defending the rights of taxpayers in situations of undue collection, challenging taxes and other fiscal measures, as well as regulating the actions of the state in supervising and collecting taxes. The aim is to ensure a balance between compliance with tax obligations and the protection of taxpayers' rights under the tax system.",
+  
+    //ACTIVITY --> COMERCIAL
+    comercialField: "Commercial and Corporate Law",
+    comercialFieldStart: " ",
+    comercialFieldEnd: "regulates economic and business activities, ensuring a stable legal framework for the creation, management and dissolution of companies. This branch of law covers the rules governing relations between traders, companies and other economic agents, promoting security and transparency in the market.",
+    comercialFieldSentence2: "Fundamental to the functioning of companies, Commercial and Corporate Law defines the rules for setting up companies, commercial contracts, the liability of partners and directors, as well as the mechanisms for resolving business disputes. Its application aims to ensure balance in commercial relations and foster a solid and competitive business environment.",
+    comercialFieldListItem1: "Commercial contracts",
+
+    //ACTIVITY --> FAMILY
+    familyField: "Family Law",
+    familyFieldStart: " ",
+    familyFieldEnd: "is the branch of law that regulates legal relations between members of a family, dealing with essential issues such as marriage, de facto unions, filiation, custody of children, adoption and succession. This field of law aims to protect the individual and collective rights of family members, promoting harmonious coexistence and the well-being of the people involved. Legislation in this area also deals with the protection of children, the elderly and other vulnerable family members.",
+    familyFieldSentence2: "In addition, Family Law deals with issues such as the property regime in marriage, the sharing of assets, alimony and the definition of parental responsibilities. The aim is to resolve family disputes in a balanced and fair manner, ensuring that the rights of each individual are respected and that the best interests of children are always a priority in judicial decisions.",
+    familyFieldListItem1: "Paternity and maintenance claims",
+    familyFieldListItem2: "Actions for Separation of Property and of Persons and Property",
+    familyFieldListItem3: "Adoption",
+    familyFieldListItem4: "Catholic marriage annulment",
+    familyFieldListItem5: "Disinheritance",
+    familyFieldListItem6: "Divorce by mutual consent and without consent of other spouse",
+    familyFieldListItem7: "Family mediation",
+    familyFieldListItem8: "Protection of Children and Young People at Risk",
+    familyFieldListItem9: "Parental Responsibilities",
   }
 };
